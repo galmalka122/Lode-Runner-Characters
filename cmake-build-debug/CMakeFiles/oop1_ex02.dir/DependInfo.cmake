0df9650ddee43949dd6a64493c3c3ef4a030110c
galmalka122 <galmalka122@gmail.com>
2020-11-24 12:58:44 +0200

    have to commit...

---
+++ cmake-build-debug/CMakeFiles/oop1_ex02.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/gal and eden/CLionProjects/keyboardExample/ex02_Gal_Malka_Yaniv_Sonino/src/Board.cpp" "C:/Users/gal and eden/CLionProjects/keyboardExample/ex02_Gal_Malka_Yaniv_Sonino/cmake-build-debug/CMakeFiles/oop1_ex02.dir/src/Board.cpp.obj"
+  "C:/Users/gal and eden/CLionProjects/keyboardExample/ex02_Gal_Malka_Yaniv_Sonino/src/Enemy.cpp" "C:/Users/gal and eden/CLionProjects/keyboardExample/ex02_Gal_Malka_Yaniv_Sonino/cmake-build-debug/CMakeFiles/oop1_ex02.dir/src/Enemy.cpp.obj"
+  "C:/Users/gal and eden/CLionProjects/keyboardExample/ex02_Gal_Malka_Yaniv_Sonino/src/GameController.cpp" "C:/Users/gal and eden/CLionProjects/keyboardExample/ex02_Gal_Malka_Yaniv_Sonino/cmake-build-debug/CMakeFiles/oop1_ex02.dir/src/GameController.cpp.obj"
+  "C:/Users/gal and eden/CLionProjects/keyboardExample/ex02_Gal_Malka_Yaniv_Sonino/src/Player.cpp" "C:/Users/gal and eden/CLionProjects/keyboardExample/ex02_Gal_Malka_Yaniv_Sonino/cmake-build-debug/CMakeFiles/oop1_ex02.dir/src/Player.cpp.obj"
+  "C:/Users/gal and eden/CLionProjects/keyboardExample/ex02_Gal_Malka_Yaniv_Sonino/src/io.cpp" "C:/Users/gal and eden/CLionProjects/keyboardExample/ex02_Gal_Malka_Yaniv_Sonino/cmake-build-debug/CMakeFiles/oop1_ex02.dir/src/io.cpp.obj"
+  "C:/Users/gal and eden/CLionProjects/keyboardExample/ex02_Gal_Malka_Yaniv_Sonino/src/main.cpp" "C:/Users/gal and eden/CLionProjects/keyboardExample/ex02_Gal_Malka_Yaniv_Sonino/cmake-build-debug/CMakeFiles/oop1_ex02.dir/src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/oop1_ex02.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/oop1_ex02.dir/src/Board.cpp.obj"
+  "CMakeFiles/oop1_ex02.dir/src/Enemy.cpp.obj"
+  "CMakeFiles/oop1_ex02.dir/src/GameController.cpp.obj"
+  "CMakeFiles/oop1_ex02.dir/src/Player.cpp.obj"
+  "CMakeFiles/oop1_ex02.dir/src/io.cpp.obj"
+  "CMakeFiles/oop1_ex02.dir/src/main.cpp.obj"
+  "oop1_ex02.exe"
+  "oop1_ex02.exe.manifest"
+  "oop1_ex02.lib"
+  "oop1_ex02.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/oop1_ex02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
